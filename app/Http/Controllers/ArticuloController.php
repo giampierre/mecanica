df3176e5c4548a84b9d5404ec2e5234582db368a
JorDey <jordyllanos22@gmail.com>
2020-06-28 01:46:35 -0500

    modi

--- app/Http/Controllers/ArticuloController.php
+++ app/Http/Controllers/ArticuloController.php
@@ -12,15 +12,17 @@ class ArticuloController extends Controller
     }
     public function crearArticulo(Request $request){
         $nuevoArticulo= new Articulo();
-        $nuevoArticulo->codigo = $request->codigo;
-        $nuevoArticulo->nombre = $request->nombre;
-        $nuevoArticulo->precio = $request->precio;
+        $nuevoArticulo->oem = $request->oem;
+        $nuevoArticulo->cod_gen = $request->cod_gen;        
         $nuevoArticulo->descripcion = $request->descripcion;
+        $nuevoArticulo->marca = $request->marca;
+        $nuevoArticulo->id_ubi = $request->id_ubi;
+        $nuevoArticulo->costo = $request->costo;
+        $nuevoArticulo->costo_igv = $request->costo_igv;
+        $nuevoArticulo->venta = $request->venta;
+        $nuevoArticulo->cantidad = $request->cantidad;
         $nuevoArticulo->estado = "1";
-
         $nuevoArticulo->save();
-
         return $nuevoArticulo;
-
     }
 }
